2424a2fd3b93441dfd6031acbbd2e58092c7abc2
nobody3007 <gaderakesh279@gmail.com>
2026-09-12 01:28:33 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (75.40%) - LeetSync

---
+++ 344-reverse-string/reverse-string.c
@@ -0,0 +1,13 @@
+void reverseString(char* s, int sSize) {
+    int i=0;
+    int j=sSize-1;
+    while(i<j)
+    {
+        char temp=s[i];
+        s[i]=s[j];
+        s[j]=temp;
+        i++;
+        j--;
+    }
+   
+}
